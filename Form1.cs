daa8e4fef9879dcbb332c6aa0f9769bfb90b25ae
Igor Khisamiev <riks@2bim.ru>
2020-01-07 21:45:11 +0300

    1

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyUtilites
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа мои утилиты", "О программе");
+        }
+    }
+}
